148d9af3a97b36353897b7a1bde170fe94f027ab
appuser <appuser@journey2-6c8fdfffd6-kkcfk>
2023-03-02 14:18:34 +0000

    starter project

---
+++ src/main/java/com/codecool/elevator/Application.java
@@ -0,0 +1,19 @@
+package com.codecool.elevator;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        // Incoming requests:
+        //
+        // Outside UP request from floor 0
+        // Inside UP request going to floor 3.
+        // Inside UP request going to floor 2.
+        // Outside DOWN request from floor 4.
+        // Outside DOWN request from floor 1.
+        // Inside DOWN request going to floor 1.
+        // Inside DOWN request going to floor 2.
+        // Inside DOWN request going to floor 0.
+
+    }
+}
